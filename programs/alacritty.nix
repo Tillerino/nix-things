0ba0c15822ab43d6d73e98a6bde839798e08ebc0
Tillerino <tillmann.gaida@gmail.com>
2023-07-06 09:48:54 +0200

    Alacritty theme

--- programs/alacritty.nix
+++ programs/alacritty.nix
@@ -1,15 +1,25 @@
+# https://github.com/nix-community/home-manager/blob/master/modules/programs/alacritty.nix
+
 { pkgs, package ? pkgs.alacritty } :
 
-{
+let melange = pkgs.fetchFromGitHub {
+  owner = "savq";
+  repo = "melange-nvim";
+  rev = "f15922543dd70b360335effb61411c05c710b99c";
+  hash = "sha256-Ad6PuVhQY4T5wcW6jV9MkSNRlcOitlfZOrfT38GuXKc=";
+};
+
+in {
   enable = true;
   package = package;
 
   settings = {
+    import = [ "${melange}/term/alacritty/melange_light.yml" ];
     font =  {
       normal = { family = "IBM Plex Mono"; weight = 150; }; # weight not working?
       bold.family = "IBM Plex Mono";
       italic.family = "IBM Plex Mono";
-      size = 16.0;
+      size = 14.0;
     };
     window.dimensions = {
       columns = 120;
