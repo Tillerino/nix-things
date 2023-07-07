8bd18aefc346155f5be7a179401d2aa0a2b8cc83
Tillerino <tillmann.gaida@gmail.com>
2023-07-07 18:11:40 +0200

    add appends to neovim config

--- programs/neovim.nix
+++ programs/neovim.nix
@@ -1,4 +1,8 @@
-{ pkgs }:
+{ pkgs, append ? { } }:
+
+let finalAppend = { plugins = [ ]; extraPackages = [ ]; extraConfig = ""; extraLuaConfig = ""; };
+
+in
 
 {
     enable = true;
@@ -11,18 +15,28 @@
       pkgs.vimPlugins.gruvbox-nvim
       pkgs.vimPlugins.melange-nvim
       pkgs.vimPlugins.telescope-nvim
-      pkgs.vimPlugins.copilot-vim # set up with :Copilot setup
-    ];
+    ] ++ finalAppend.plugins;
 
     extraPackages = [
       pkgs.dhall-lsp-server
       pkgs.nil
       pkgs.java-language-server
-    ];
+    ] ++ finalAppend.extraPackages;
 
     extraConfig = ''
-      '';
+      " https://vi.stackexchange.com/a/4175
+      " tabstop:          Width of tab character
+      " softtabstop:      Fine tunes the amount of white space to be added
+      " shiftwidth        Determines the amount of whitespace to add in normal mode
+      " expandtab:        When this option is enabled, vi will use spaces instead of tabs
+      set tabstop     =2
+      set softtabstop =2
+      set shiftwidth  =2
+      set expandtab
+    '' + finalAppend.extraConfig;
 
-    extraLuaConfig = "dofile('${./neovim.lua}')";
+    extraLuaConfig = ''
+      dofile('${./neovim.lua}')
+    '' + finalAppend.extraLuaConfig;
 }
 
