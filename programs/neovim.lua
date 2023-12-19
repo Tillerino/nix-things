74bc53cfcc0d64760bcc4def4deebe0e977c208b
Tillerino <tillmann.gaida@gmail.com>
2023-12-19 20:10:14 +0100

    neovim: find hidden files

--- programs/neovim.lua
+++ programs/neovim.lua
@@ -30,7 +30,9 @@ vim.keymap.set("n", "<leader>hms", ":w <CR> :!home-manager switch <CR>")
 -- Telescope
 local builtin = require('telescope.builtin')
 -- see https://github.com/nvim-telescope/telescope.nvim#pickers
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>ff', function()
+  builtin.find_files{hidden = true}
+end, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
