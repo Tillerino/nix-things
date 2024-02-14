4b1dc452d275baa1b9ed58674777bd3dc0bd5064
Tillmann Gaida <tillmann.gaida@goto10ug.de>
2024-02-14 11:05:04 +0100

    eclipse: more favorites

--- scripts/setup-eclipse-workspace.sh
+++ scripts/setup-eclipse-workspace.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
 set -e
+
+if [ ! -d .metadata ]; then
+  echo WARN: folder .metadata does not exist. Are you in the correct directory?
+fi
+
 mkdir -p ./.metadata/.plugins/org.eclipse.core.runtime/.settings
 
 cat <<EOT >> ./.metadata/.plugins/org.eclipse.core.runtime/.settings/org.eclipse.m2e.apt.prefs
@@ -9,7 +14,7 @@ org.eclipse.m2e.apt.mode=jdt_apt
 EOT
 
 cat <<EOT >> ./.metadata/.plugins/org.eclipse.core.runtime/.settings/org.eclipse.jdt.ui.prefs
-content_assist_favorite_static_members=org.assertj.core.api.Assertions.*;org.mockito.Mockito.*;java.util.stream.Collectors.*;org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
+content_assist_favorite_static_members=org.assertj.core.api.Assertions.*;org.mockito.Mockito.*;java.util.stream.Collectors.*;org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*
 EOT
 
 cat <<EOT >> ./.metadata/.plugins/org.eclipse.core.runtime/.settings/org.eclipse.ui.editors.prefs
