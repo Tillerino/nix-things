38b5878b1859eac0b94f4f1e35291eba3de03fcc
Tillmann Gaida <tillmann.gaida@gmail.com>
2024-03-31 14:37:00 +0200

    mvnd: simplify derivation

--- pkgs/mvnd/default.nix
+++ pkgs/mvnd/default.nix
@@ -11,18 +11,12 @@ maven.buildMavenPackage rec {
     hash = "sha256-tC1nN81aimfA0CWQAU6J/QEXO2mmSQln+dkiB2jyqfI=";
   };
 
-  mvnHash = "sha256-duqKRgU8ubcIxYskHxZMv7sE+h3l5VgMvdIDsNu44Io=";
+  mvnHash = "sha256-YeFSUj12utA7dNncSa65uMqHSsrPly2aE3n2sI1D8Nk=";
 
   buildOffline = true;
-  mvnDepsParameters = "-B -Pnative";
+  mvnDepsParameters = "-B -Pnative de.qaware.maven:go-offline-maven-plugin:1.2.8:resolve-dependencies -DdownloadSources";
   manualMvnArtifacts = [
-    "org.codehaus.mojo:extra-enforcer-rules:1.7.0"
-    "org.apache.groovy:groovy-all:4.0.13:pom"
-    "org.apache.maven.shared:maven-shared-resources:5"
     "org.apache.apache.resources:apache-jar-resource-bundle:1.5"
-    "org.apache.maven:maven-slf4j-wrapper:4.0.0-alpha-8:jar:sources"
-    "org.slf4j:slf4j-simple:1.7.36:jar:sources"
-    "com.thoughtworks.xstream:xstream:1.4.20"
     "org.apache.maven:apache-maven:3.9.5:tar.gz:bin"
     "org.apache.maven:apache-maven:4.0.0-alpha-8:tar.gz:bin"
   ];
