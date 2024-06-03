9d25ea7dbfc9e431b8cc76359c9d2fe6f1879e87
Tillmann Gaida <tillmann.gaida@goto10ug.de>
2024-06-03 19:25:25 +0200

    trying to get mvnd to build

--- pkgs/mvnd/default.nix
+++ pkgs/mvnd/default.nix
@@ -2,23 +2,23 @@
 
 maven.buildMavenPackage rec {
   pname = "mvnd";
-  version = "1.0-m8";
+  version = "mvnd-1.x";
 
   src = fetchFromGitHub {
     owner = "apache";
     repo = "maven-mvnd";
-    rev = "${version}";
-    hash = "sha256-tC1nN81aimfA0CWQAU6J/QEXO2mmSQln+dkiB2jyqfI=";
+    rev = "42e5786701308968e312e335354deffa17f3ce4b";
+    hash = "sha256-61dYN9jhxOYbyGWD8n6Gm7hLX5I8P8BbURBs5HvuBRk=";
   };
 
-  mvnHash = "sha256-YeFSUj12utA7dNncSa65uMqHSsrPly2aE3n2sI1D8Nk=";
+  mvnHash = "sha256-5UAk5YDczKqV2OLbkF6buIp2LwW0ye9yH/S+PMN40cM=";
 
   buildOffline = true;
   mvnDepsParameters = "-B -Pnative de.qaware.maven:go-offline-maven-plugin:1.2.8:resolve-dependencies -DdownloadSources";
   manualMvnArtifacts = [
     "org.apache.apache.resources:apache-jar-resource-bundle:1.5"
-    "org.apache.maven:apache-maven:3.9.5:tar.gz:bin"
-    "org.apache.maven:apache-maven:4.0.0-alpha-8:tar.gz:bin"
+    "org.apache.maven:apache-maven:3.9.7:tar.gz:bin"
+    "org.apache.maven:maven-slf4j-provider:3.9.7:jar:sources"
   ];
 
   mvnParameters = "-B -Pnative -DskipTests=true -Dmaven.buildNumber.skip=true -Drat.skip=true -Denforcer.skip=true -Dspotless.skip=true";
@@ -28,7 +28,7 @@ maven.buildMavenPackage rec {
   installPhase = ''
     mkdir -p $out/mvnd
     mkdir -p $out/bin
-    cp -r dist-m39/target/maven-mvnd-1.0-m8-m39-linux-amd64/. $out/mvnd
+    cp -r dist/target/maven-mvnd-1.0-m9-SNAPSHOT-linux-amd64/. $out/mvnd
     ln -s $out/mvnd/bin/mvnd $out/bin/mvnd
 
     runHook postInstall
