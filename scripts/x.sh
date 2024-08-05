09e7e1fe6daffdcfd844690f608c5c30d1affdcb
Tillmann Gaida <tillmann.gaida@goto10ug.de>
2024-08-05 11:02:40 +0200

    x: nicer output switcher

--- scripts/x.sh
+++ scripts/x.sh
@@ -5,21 +5,9 @@ set -euo pipefail
 if [ "$XDG_SESSION_TYPE" = "tty" ]; then
   DMENU=(fzf --no-sort --layout reverse --header)
   NOTIFY=(echo)
-
-  eq="(in terminal)"
 else
   DMENU=(dmenu "-l" 20 "-i" "-fn" 'DejaVu Sans Mono-19' "-nf" '#b6b7b8' "-sf" '#262626' "-nb" '#262626' "-sb" '#b6b7b8' "-p")
   NOTIFY=(notify-send -e -t 500)
-
-  if pgrep easyeffects; then
-    if easyeffects -b 3 | grep 0; then
-      eq="Bypass EQ";
-    else
-      eq="Enable EQ";
-    fi
-  else
-    eq="(Easyeffects not running)"
-  fi
 fi
 
 function error() {
@@ -29,12 +17,31 @@ function error() {
 
 trap error ERR
 
-l1=$(echo "Sound" | "${DMENU[@]}" "Level 1" || exit 0)
+# so we can go `kill -INT $$` in subshells. That will exit the script
+trap 'exit 0' INT
+
+l1=$(echo "Sound" | "${DMENU[@]}" "Level 1" || kill -INT $$)
 
 case $l1 in
   "Sound")
+    if [ "$XDG_SESSION_TYPE" = "tty" ]; then
+      eq="(in terminal)"
+      output="(in terminal)"
+    else
+      if pgrep easyeffects; then
+        if easyeffects -b 3 | grep 0; then
+          eq="Bypass EQ";
+        else
+          eq="Enable EQ";
+        fi
+      else
+        eq="(Easyeffects not running)"
+      fi
+      output="Output ($(pactl info | rg "Default Sink: (.*)" -or '$1'))"
+    fi
+
     l2=$(echo "$eq"'
-Output' | "${DMENU[@]}" "Level 2" || exit 0)
+'"$output" | "${DMENU[@]}" "Level 2" || kill -INT $$)
     case $l2 in
       "$eq")
         if easyeffects -b 3 | grep 0; then
@@ -43,8 +50,8 @@ Output' | "${DMENU[@]}" "Level 2" || exit 0)
           easyeffects -b 2 && "${NOTIFY[@]}" "EasyEffects: Enabled";
         fi;
         ;;
-      "Output")
-        device=$(pactl list sinks | rg "Name: (.*)" -or '$1' | "${DMENU[@]}" "Choose Device")
+      "$output")
+        device=$(pactl list sinks | rg "Name: (.*)" -or '$1' | rg -v easyeffects | "${DMENU[@]}" "Choose Device" || kill -INT $$)
         pactl set-default-sink "$device"
         ;;
       *)
