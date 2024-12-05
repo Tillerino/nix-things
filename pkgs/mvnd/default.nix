ff173b19ba80dce8582c65789008c5ce6c318a22
Tillmann Gaida <tillmann.gaida@goto10ug.de>
2024-12-05 08:03:20 +0100

    fugde mvnd based on open MR:
    https://github.com/NixOS/nixpkgs/pull/337151

--- pkgs/mvnd/default.nix
+++ pkgs/mvnd/default.nix
@@ -1,34 +1,45 @@
-{ fetchFromGitHub, maven, graalvmCEPackages }:
+{ fetchFromGitHub, maven, graalvmCEPackages, lib }:
 
 maven.buildMavenPackage rec {
   pname = "mvnd";
-  version = "mvnd-1.x";
+  version = "1.0.2";
 
   src = fetchFromGitHub {
     owner = "apache";
     repo = "maven-mvnd";
-    rev = "42e5786701308968e312e335354deffa17f3ce4b";
-    hash = "sha256-61dYN9jhxOYbyGWD8n6Gm7hLX5I8P8BbURBs5HvuBRk=";
+    rev = version;
+    hash = "sha256-c1jD7m4cOdPWQEoaUMcNap2zvvX7H9VaWQv8JSgAnRU=";
   };
 
-  mvnHash = "sha256-5UAk5YDczKqV2OLbkF6buIp2LwW0ye9yH/S+PMN40cM=";
+  mvnHash = "sha256-Bx0XSnpHNxNX07uVPc18py9qbnG5b3b7J4vs44ty034=";
 
-  buildOffline = true;
-  mvnDepsParameters = "-B -Pnative de.qaware.maven:go-offline-maven-plugin:1.2.8:resolve-dependencies -DdownloadSources";
-  manualMvnArtifacts = [
-    "org.apache.apache.resources:apache-jar-resource-bundle:1.5"
-    "org.apache.maven:apache-maven:3.9.7:tar.gz:bin"
-    "org.apache.maven:maven-slf4j-provider:3.9.7:jar:sources"
+  #buildOffline = true;
+  mvnJdk = graalvmCEPackages.graalvm-ce;
+  mvnDepsParameters = mvnParameters;
+
+  mvnParameters = lib.concatStringsSep " " [
+    "-Dmaven.buildNumber.skip=true" # skip build number generation; requires a git repository
+    "-Drat.skip=true" # skip license checks; they require manaul approval and should have already been run upstream
+    "-Dspotless.skip=true" # skip formatting checks
+
+    # skip tests that fail in the sandbox
+    "-pl"
+    "!integration-tests"
+    "-Dtest=!org.mvndaemon.mvnd.client.OsUtilsTest,!org.mvndaemon.mvnd.cache.impl.CacheFactoryTest"
+    "-Dsurefire.failIfNoSpecifiedTests=false"
+
+    "-Pnative"
+    # propagate linker args required by the darwin build
+    # see `buildGraalvmNativeImage`
+    ''-Dgraalvm-native-static-opt="-H:-CheckToolchain $(export -p | sed -n 's/^declare -x \([^=]\+\)=.*$/ -E\1/p' | tr -d \\n)"''
   ];
-
-  mvnParameters = "-B -Pnative -DskipTests=true -Dmaven.buildNumber.skip=true -Drat.skip=true -Denforcer.skip=true -Dspotless.skip=true";
 
   nativeBuildInputs = [ graalvmCEPackages.graalvm-ce ];
 
   installPhase = ''
     mkdir -p $out/mvnd
     mkdir -p $out/bin
-    cp -r dist/target/maven-mvnd-1.0-m9-SNAPSHOT-linux-amd64/. $out/mvnd
+    cp -r dist/target/maven-mvnd-$version-linux-amd64/. $out/mvnd
     ln -s $out/mvnd/bin/mvnd $out/bin/mvnd
 
     runHook postInstall
