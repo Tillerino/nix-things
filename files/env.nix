41341c722c0bec25428030626d4cc649b0f63737
Tillmann Gaida <tillmann.gaida@gmail.com>
2025-10-07 14:46:04 +0200

    env is now a function

--- files/env.nix
+++ files/env.nix
@@ -1,6 +1,6 @@
 # These go into .nix-profile/etc/profile.d/hm-session-vars.sh
 # Log back in to reload!
-{
+{ config } : {
   EDITOR = "nvim";
   MAVEN_OPTS = "-Daether.dependencyCollector.impl=bf";
 }
